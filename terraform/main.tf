8c4b852d8fa9faa84f8426ab0c6641bfa1aef8e9
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2024-10-26 18:45:52 +0100

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,49 +37,15 @@ resource "aws_security_group" "eks_cluster_sg" {
   }
 }
 
-# resource "aws_security_group" "eks_worker_sg" {
-#   name        = "eks-worker-sg-${var.cluster_name}"
-#   description = "Security group for EKS worker nodes ${var.cluster_name}"
-#   vpc_id      = var.vpc_id  # Utilisation de la variable pour l'ID du VPC
-#
-#   ingress {
-#     from_port = 8083
-#     to_port   = 8083
-#     protocol  = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   ingress {
-#     from_port = 30000
-#     to_port   = 30000
-#     protocol  = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   egress {
-#     from_port = 0
-#     to_port   = 0
-#     protocol  = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   tags = {
-#     Name = "eks-worker-sg-${var.cluster_name}"
-#   }
-# }
-
 resource "aws_eks_cluster" "my_cluster" {
   name     = var.cluster_name
   role_arn = var.role_arn
   version  = "1.30"
 
   vpc_config {
-    subnet_ids = var.subnet_ids
-    security_group_ids = [
-      aws_security_group.eks_cluster_sg.id,
-    ]
+    subnet_ids         = var.subnet_ids
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
-
 }
 
 resource "aws_eks_node_group" "my_node_group" {
@@ -93,9 +59,4 @@ resource "aws_eks_node_group" "my_node_group" {
     max_size     = 3
     min_size     = 1
   }
-
-  # remote_access {
-  #   # ec2_ssh_key = "my-key"
-  #   source_security_group_ids = [aws_security_group.eks_worker_sg.id]
-  # }
 }

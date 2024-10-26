68ec20b69936da3f87162ad3177697eda677642a
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2024-10-26 19:44:59 +0100

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "aws_security_group_rule" "allow_all_inbound_on_8083" {
   from_port         = 8083
   to_port           = 8083
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_eks_cluster.my_cluster.vpc_config[0].cluster_security_group_id
 }
 
@@ -26,7 +26,7 @@ resource "aws_security_group_rule" "allow_all_inbound_on_30000" {
   from_port         = 30000
   to_port           = 30000
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_eks_cluster.my_cluster.vpc_config[0].cluster_security_group_id
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,35 +1,23 @@
 variable "aws_region" {
-  description = "La région AWS"
+  description = "AWS Region"
   type        = string
   default     = "us-east-1"  # Région mise à jour
 }
 
 variable "cluster_name" {
-  description = "Nom du cluster EKS"
+  description = "EKS Cluster Name"
   type        = string
   default     = "mykubernetes"  # Nom du cluster mis à jour
 }
 
 variable "subnet_ids" {
-  description = "IDs des sous-réseaux"
+  description = "Subnet IDs"
   type        = list(string)
   default     = ["subnet-0946253780c6f471e", "subnet-061b8dfa84c05026a"]  # Valeurs par défaut
 }
 
 variable "role_arn" {
-  description = "ARN du rôle IAM pour EKS"
+  description = "IAM Role ARN for EKS"
   type        = string
   default     = "arn:aws:iam::985365036259:role/LabRole"  # Valeur par défaut
-}
-
-variable "vpc_id" {
-  description = "L'ID du VPC pour le cluster EKS"
-  type        = string
-  default     = "vpc-00cf47e7743508002"  # Remplacez par votre ID de VPC réel
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"  # Modifiez-le selon vos besoins
 }
